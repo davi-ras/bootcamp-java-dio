4cceae99a39fd3dddd6fed122d509bdd14f56226
Davi <rxtd99@hotmail.com>
2026-07-04 02:41:57 -0300

    feat: adiciona exercicios do modulo01

---
+++ src/exercicios/modulo01_fundamentos/Exercicio01.java
@@ -0,0 +1,23 @@
+package exercicios.modulo01_fundamentos;
+
+import java.time.LocalDate;
+import java.time.chrono.ChronoLocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class Exercicio01 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        LocalDate date = LocalDate.now();
+        int ano = date.getYear();
+
+        System.out.println("Digite seu nome: ");
+        String nome = entrada.nextLine();
+
+        System.out.println("Digite o seu ano de nascimento: ");
+        int ano_nasc = ano - entrada.nextInt();
+
+        System.out.println("Ola " + nome + " voce tem " + ano_nasc + " anos");
+        entrada.close();
+    }
+}
