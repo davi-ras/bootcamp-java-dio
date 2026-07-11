0f11544ced8e3091806cc108b55a8ccbee652937
Davi <rxtd99@hotmail.com>
2026-07-10 21:34:44 -0300

    feat: adiciona exercicio01 do modulo B

--- src/exercicios/Bestruturascontrole/Exercicio01.java
+++ src/exercicios/Bestruturascontrole/Exercicio01.java
@@ -1,7 +1,16 @@
 package exercicios.Bestruturascontrole;
 
+import java.util.Scanner;
+
 public class Exercicio01 {
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
 
+        System.out.print("Digite um número inteiro: ");
+        int numero = entrada.nextInt();
+
+        for (int i = 1; i < 11; i++) {
+            System.out.println(numero + " x " + i + "\t= " + (i * numero));
+        }
     }
 }
